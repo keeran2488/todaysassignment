99c57c0662dd25b5fb5d9cf01dc5f667c65c2e55
keeran2488 <49069135+keeran2488@users.noreply.github.com>
2020-02-11 17:38:04 +0545

    added signup form

--- lib/auth.dart
+++ lib/auth.dart
@@ -7,55 +7,104 @@ class LogInForm extends StatefulWidget {
   _LogInFormState createState() => _LogInFormState();
 }
 
+enum FormType{
+  login,
+  register,
+}
+
 class _LogInFormState extends State<LogInForm> {
   String _email, _password;
+  FormType _formType = FormType.login;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void moveToRegister(){
+    _formKey.currentState.reset();
+    setState((){
+      _formType = FormType.register;
+    });
+  }
+
+  void moveToSignIn(){
+    _formKey.currentState.reset();
+    setState((){
+      _formType = FormType.login;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Login Form"),
       ),
-      body: Form(
+      body: Container(
+        padding: EdgeInsets.fromLTRB(30.0, 30.0, 40.0, 30.0),
+        child: Form(
         key: _formKey,
         child: Column(
-            children: <Widget>[
-              TextFormField(
-                validator: (input){
-                  if(input.isEmpty) {
-                    return "Please type an email.";
-                  }else if(!input.contains("@")){
-                    return "Enter a valid email.";
-                  }
-                  return null;
-                },
-                onSaved: (input) => _email = input,
-                decoration: InputDecoration(
-                    labelText: 'Email'
-                ),
-              ),
-              TextFormField(
-                validator: (input){
-                  if(input.isEmpty){
-                    return 'Your password needs to be atleast 6 characters.';
-                  }
-                  return null;
-                },
-                onSaved: (input) => _password = input,
-                decoration: InputDecoration(
-                    labelText: 'Password'
-                ),
-                obscureText: true,
-              ),
-              RaisedButton(
-                onPressed: signIn,
-                child: Text("Sign in"),
-              )
-            ]
+            children: buildInputs() + buildButtons()
         ),
       ),
+    )
     );
   }
+
+  List<Widget> buildInputs(){
+    return <Widget>[
+      TextFormField(
+        validator: (input){
+          if(input.isEmpty) {
+            return "Please type an email.";
+          }else if(!input.contains("@")){
+            return "Enter a valid email.";
+          }
+          return null;
+        },
+        onSaved: (input) => _email = input,
+        decoration: InputDecoration(
+            labelText: 'Email'
+        ),
+      ),
+      TextFormField(
+        validator: (input){
+          if(input.isEmpty){
+            return 'Your password needs to be atleast 6 characters.';
+          }
+          return null;
+        },
+        onSaved: (input) => _password = input,
+        decoration: InputDecoration(
+            labelText: 'Password'
+        ),
+        obscureText: true,
+      ),
+    ];
+  }
+
+  List<Widget> buildButtons(){
+    if(_formType == FormType.login){
+      return <Widget>[
+      RaisedButton(
+        onPressed: signIn,
+        child: Text("Sign in"),
+      ),
+      FlatButton(
+        onPressed: moveToRegister, 
+        child: Text("Doesn't have account? Sign up"),
+      )
+    ];
+    }
+    return <Widget>[
+      RaisedButton(
+        onPressed: signIn,
+        child: Text("Sign up"),
+      ),
+      FlatButton(
+        onPressed: moveToSignIn, 
+        child: Text("Already have account? Sign in"),
+      )
+    ];
+}
 
   Future<void> signIn() async{
     final formState = _formKey.currentState;
