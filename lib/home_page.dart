1d4ddaa9e235b20c97ab6e803248da4570d4d86a
sijan joshi <33340684+joshisijan@users.noreply.github.com>
2020-02-15 14:59:52 +0545

    edited drawer header and drawer

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:todaysassignment/functions/constants.dart';
 import 'package:todaysassignment/routine.dart';
 
 import 'assignment.dart';
+import 'custom_plugin/drawer_button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({this.onSignedOut});
@@ -13,7 +15,7 @@ class HomePage extends StatefulWidget {
     try {
       await FirebaseAuth.instance.signOut();
       Navigator.of(context).pushReplacementNamed('/');
-    }catch(e){
+    } catch (e) {
       print(e);
     }
   }
@@ -25,8 +27,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
@@ -37,18 +41,66 @@ class _HomePageState extends State<HomePage> {
         length: 2,
         child: Scaffold(
           drawer: Drawer(
-            child: ListView(
-              padding: EdgeInsets.zero,
+            child: Column(
               children: <Widget>[
-                DrawerHeader(
-                  child: Text("Choose you preferences"),
-                  decoration: BoxDecoration(
-                    color: Colors.teal,
+                Expanded(
+                  child: ListView(
+                    padding: EdgeInsets.zero,
+                    children: <Widget>[
+                      FutureBuilder(
+                        builder: (context, snapshot){
+                          if(snapshot.connectionState == ConnectionState.waiting){
+                            return CircularProgressIndicator();
+                          }else{
+                            return UserAccountsDrawerHeader(
+                              accountName: Text(snapshot.data.displayName.toString()),
+                              accountEmail: Text(snapshot.data.email.toString()),
+                              currentAccountPicture: CircleAvatar(
+                                backgroundImage: NetworkImage('https://picsum.photos/id/237/200/300'),
+                              ),
+                            );
+                          }
+                        },
+                        future: FirebaseAuth.instance.currentUser(),
+                      ),
+                      DrawerButton(
+                        title: 'Profile',
+                        icon: Icons.account_circle,
+                        onclick: () {
+
+                        },
+                      ),
+                      DrawerButton(
+                        title: 'Settings',
+                        icon: Icons.settings,
+                        onclick: () {
+
+                        },
+                      ),
+                      DrawerButton(
+                        title: 'Contact us',
+                        icon: Icons.email,
+                        onclick: () {
+
+                        },
+                      ),
+                      DrawerButton(
+                        title: 'About us',
+                        icon: Icons.help,
+                        onclick: () {
+
+                        },
+                      ),
+                      SizedBox(
+                        height: kDefaultPadding,
+                      ),
+                    ],
                   ),
                 ),
-                ListTile(
-                  title: Text("Sign Out"),
-                  onTap: (){
+                DrawerButton(
+                  title: 'Sign Out',
+                  icon: Icons.close,
+                  onclick: () {
                     widget.signOut(context);
                   },
                 ),
